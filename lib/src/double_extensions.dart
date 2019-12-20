24236b3892021f8a05c29ebda19d9b8ce64f1671
Sasha Prokhorenko <djminikin@gmail.com>
2019-12-20 10:49:35 +0100

    Clean up

--- lib/src/double_extensions.dart
+++ lib/src/double_extensions.dart
@@ -1,39 +1,6 @@
 import 'dart:math';
 
 extension DoubleExtention on double {
-  /// Calculate the circumference of a circle with the given [radius].
-  ///
-  /// ```dart
-  /// print(5.0.calculateCircumference) -> 31.41592653589793
-  /// ```
-  ///
-  double get calculateCircumference => 2 * pi * this;
-
-  /// Calculate the area of a circle with the given [radius].
-  ///
-  /// ```dart
-  /// print(5.0.calculateCircularArea) -> 78.53981633974483s
-  /// ```
-  ///
-  double get calculateCircularArea => pi * (this * this);
-
-  /// Calculate the diameter of a circle with the given [radius].
-  ///
-  /// ```dart
-  /// print(5.0.calculateCircleDiameter) -> 10.0
-  /// ```
-  ///
-  double get calculateCircleDiameter => 2 * this;
-
-  /// Calculate the area of a square or rectangle with length [a] and/or length[b].
-  ///
-  /// ```dart
-  /// print(2.0.calculateSquareArea()) -> 4.0
-  /// print(2.0.calculateSquareArea(b: 4.6)) -> 9.2
-  /// ```
-  ///
-  double calculateSquareArea({double b}) => b == null ? this * this : this * b;
-
   /// Rounds the give double [value] to the given [decimals].
   ///
   /// ```dart
--- test/double_extensions_test.dart
+++ test/double_extensions_test.dart
@@ -4,23 +4,6 @@ import 'package:darty/darty.dart';
 
 void main() {
   group('Double Extensions', () {
-    test('Test calculateCircumference', () {
-      expect(5.0.calculateCircumference, 31.41592653589793);
-    });
-
-    test('Test calculateCircularArea', () {
-      expect(5.0.calculateCircularArea, 78.53981633974483);
-    });
-
-    test('Test calculateCircleDiameter', () {
-      expect(5.0.calculateCircleDiameter, 10.0);
-    });
-
-    test('Test calculateSquareArea', () {
-      expect(2.0.calculateSquareArea(), 4.0);
-      expect(2.0.calculateSquareArea(b: 4.6), 9.2);
-    });
-
     test('Test round', () {
       expect(5.2832883723.rounded(2), 5.28);
       expect(5.2832883723.rounded(4), 5.2833);
